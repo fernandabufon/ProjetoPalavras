c1c57ab53365a8ede8ee732a1289a26d5a9eda3a
Fernanda Bufon <fernandab.farber@gmail.com>
2022-07-07 12:55:12 -0300

    versão final

--- palavras.py
+++ palavras.py
@@ -1,22 +1,22 @@
 import numpy as np
 import string
 from random import choice
-def main():
 
+def main():
     p, arr, o, g, a, c, m = variaveis()
     resp = np.array(palavras(p, arr, o, g, a, c, m))
     imprimir(palavras(p, arr, o, g, a, c, m), resp)
 
-
 def variaveis():
     p = "PROGRAMAÇÃO"
-    arr = np.array([['-'] * 8] * 11)  # criando uma matriz com 8 colunas e 11 linhas
+    arr = np.array([['.'] * 8] * 11)  # criando uma matriz com 8 colunas e 11 linhas
     o = "ORACLE"
     g = "GOOGLE"
     a = "APACHE"
     c = "CISCO"
     m = "META"
     return p, arr, o, g, a, c, m
+
 
 def palavras(p, arr, o, g, a, c, m):
     for pos, char in enumerate(p):
@@ -36,7 +36,7 @@ def palavras(p, arr, o, g, a, c, m):
 def gerar_letras(arr):
     for l in range(0, 11):
         for c in range(0, 8):
-            if arr[l, c] == '-':
+            if arr[l, c] == '.':
                 arr[l, c] = choice(string.ascii_letters).upper()
     return arr
 
